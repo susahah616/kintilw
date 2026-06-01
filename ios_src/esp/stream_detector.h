dac3ffaf6693b0c2f6eaa93060caf241eb2f9c02
unknown <susahah616@gmail.com>
2026-06-01 11:21:19 +0700

    Update project

--- ios_src/esp/stream_detector.h
+++ ios_src/esp/stream_detector.h
@@ -2,31 +2,42 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-namespace StreamDetector {
-    
-    inline bool IsScreenRecording() {
-        #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
-        if (@available(iOS 11.0, *)) {
-            return UIScreen.mainScreen.isCaptured;
+static inline bool StreamDetector_IsScreenRecording() {
+    #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
+    if (@available(iOS 11.0, *)) {
+        return UIScreen.mainScreen.isCaptured;
+    }
+    #endif
+    return false;
+}
+
+static inline bool StreamDetector_IsAirPlayActive() {
+    #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 50000
+    if (@available(iOS 5.0, *)) {
+        for (UIScreen *screen in UIScreen.screens) {
+            if (screen != UIScreen.mainScreen && screen.mirroredScreen == UIScreen.mainScreen) {
+                return true;
+            }
         }
-        #endif
-        return false;
+    }
+    #endif
+    return false;
+}
+
+static inline bool StreamDetector_IsStreaming() {
+    return StreamDetector_IsScreenRecording() || StreamDetector_IsAirPlayActive();
+}
+
+namespace StreamDetector {
+    inline bool IsScreenRecording() {
+        return StreamDetector_IsScreenRecording();
     }
     
     inline bool IsAirPlayActive() {
-        #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 50000
-        if (@available(iOS 5.0, *)) {
-            for (UIScreen *screen in UIScreen.screens) {
-                if (screen != UIScreen.mainScreen && screen.mirroredScreen == UIScreen.mainScreen) {
-                    return true;
-                }
-            }
-        }
-        #endif
-        return false;
+        return StreamDetector_IsAirPlayActive();
     }
     
     inline bool IsStreaming() {
-        return IsScreenRecording() || IsAirPlayActive();
+        return StreamDetector_IsStreaming();
     }
 }
